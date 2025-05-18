d83d837073df65f6c816d60d5db779abbfe1dfb2
mahmutsyilmz <mahmut14sami15@gmail.com>
2025-05-18 20:03:57 +0300

    Configure H2 in-memory database for tests with 'test' profile

--- src/test/java/com/yilmaz/goalCast/GoalCastApplicationTests.java
+++ src/test/java/com/yilmaz/goalCast/GoalCastApplicationTests.java
@@ -2,8 +2,10 @@ package com.yilmaz.goalCast;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GoalCastApplicationTests {
 
 	@Test
